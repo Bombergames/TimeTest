a59ae28dd49361cbbf7ea25eea3f71c078f8fc67
John Blum <jblum@gatech.edu>
2017-03-08 02:05:31 -0500

    Initial

---
+++ Assets/applyInitialVelocity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class applyInitialVelocity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -50000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
